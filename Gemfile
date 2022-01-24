67e195d518245f10448d80d3802c226f7cd7475c
Carl Noval <cjrnoval22@gmail.com>
2022-01-24 19:05:37 +0900

    lw minimal template + devise, just added pundit

--- Gemfile
+++ Gemfile
@@ -16,7 +16,8 @@ gem 'webpacker', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.7'
+# commented out cause used scaffold
+# gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
@@ -33,6 +34,7 @@ gem 'devise'
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'pundit'
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
   gem 'dotenv-rails'
---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,63 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    # added manually not by scaffold
+    @restaurant.user = current_user
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /restaurants/1
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      # this is because scaffold doesn't know that user_id will be handled with devise
+      # params.require(:restaurant).permit(:name, :user_id)
+      # modified value
+      params.require(:restaurant).permit(:name)
+    end
+end
